35ae03aa38d7f62a7aacdf82c614af61d0cc23d0
Daniel Spoladori <daniel.spoladori@gmail.com>
2023-11-13 09:16:45 -0300

    Dev Build (git mess fix)

--- lib/core/04-OS.lua
+++
@@ -1,6 +0,0 @@
-function wait(seconds)
-    local start = computer.uptime()
-    while computer.uptime() - start < seconds do
-        computer.pullSignal(seconds - (computer.uptime() - start))
-    end
-end
