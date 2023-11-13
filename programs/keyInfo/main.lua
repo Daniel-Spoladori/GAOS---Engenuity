2ea49598610760259ab4ea10e6523b1d944684d7
Daniel Spoladori <daniel.spoladori@gmail.com>
2023-11-13 11:43:19 -0300

    updated for close

--- programs/keyInfo/main.lua
+++ programs/keyInfo/main.lua
@@ -2,12 +2,14 @@ print("Press any key to get info about it")
 while true do
 local state, kbrd, ascii, keyPos = computer.pullSignal()
 clearTerminal()
-OS.currentLine = 0
+OS.currentLine = 1
     print("Keyboard: "..kbrd)
     print("State: "..state)
     print("ASCII: "..ascii)
     print("keyPosition: "..keyPos)
     print("")
-    print("")
-    print("")
+    print("Press 'esc' to exit")
+    if keyPos == 1 then
+        break
+    end
 end
