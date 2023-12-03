2eda133d1280dce2d2e24e79caefff27acc3986e
Daniel Spoladori <daniel.spoladori@gmail.com>
2023-12-03 10:49:26 -0300

    Changed how filesystem works and small patches

--- programs/change-dir/main.lua
+++ programs/change-dir/main.lua
@@ -1,1 +1,45 @@
-print("Write the directory bellow:")
+-- cd isnt used anymore since it is not very necessary, well thats is it it
+
+-- increment that count of time trying fix it by 1 at every hour
+-- hours trying make this work [10] (study included)
+
+print("Change Dir is decrapted!")
+
+
+-- for i, v in pairs(component.methods(address)) do
+--     -- print(i)
+-- end
+
+-- function TermCannonical(directory, paramaters)
+--     directory = tostring(directory)
+--     print("diretorio = "..directory)
+--     local segments = TermSegment(directory)
+--     local runned = false
+
+--     for index, value in pairs(segments) do
+
+--         if value == "." then
+--             table.remove(segments,index) -- "kills" the .
+--         end
+
+
+--         if value == ".." then
+--             table.remove(segments,index) -- "kills" the ..
+--             table.remove(segments,index-1) -- "kills" the previus path
+
+--         end
+
+--     end
+--     return segments
+-- end
+-- -- print("Write the directory bellow:")
+-- print("Currently at: "..terminal.path.path)
+
+-- for  i,v in pairs(TermCannonical("bin/xuxu/../documents")) do
+--     print(v)
+-- end
+
+
+-- -- local directory = terminalInput(true)
+
+-- -- print("You moved to: "..terminal.path.path)
--- programs/list-files/main.lua
+++ programs/list-files/main.lua
@@ -1,3 +1,24 @@
-for i,v in pairs(invoke(address,"list",terminal.path.path)) do
-    print("↳ " ..v)
+print("Type the directory for listing: ")
+local input = terminalInput(true) or "/"
+if invoke(address, "exists", input) then
+    
+    for i,v in pairs(invoke(address,"list",input)) do
+        print("  ↳ " ..v)
+    end
+
+else
+    print("Path not found!")
 end
+
+
+-- old list file (use if you are using CD as it is fully compatible)
+
+-- if invoke(address, "exists", terminal.) then
+    
+--     for i,v in pairs(invoke(address,"list",terminal.path.path)) do
+--         print("  ↳ " ..v)
+--     end
+
+-- else
+--     print("Path not found!")
+-- end
